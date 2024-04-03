7a2fe7a3514866623f5e681d36856796b4761b62
SairGarcia <sagab39@gmail.com>
2024-04-03 17:21:31 -0500

    adding adapter pattern route to their exclusive branch

---
+++ src/main/java/adapter/Warrior.java
@@ -0,0 +1,14 @@
+package adapter;
+
+public class Warrior implements Enemy{
+
+    @Override
+    public void attack() {
+        System.out.println("Warrior Attack!!");
+    }
+
+    @Override
+    public void damage() {
+        System.out.println("Warrior Damage!!");
+    }
+}
